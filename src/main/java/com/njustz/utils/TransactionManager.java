dbadc36379422b6f366bad16cf7b8fe852819000
njustcsezw <2990629037@qq.com>
2019-04-23 20:28:12 +0800

    second

--- src/main/java/com/njustz/utils/TransactionManager.java
+++ src/main/java/com/njustz/utils/TransactionManager.java
@@ -1,4 +1,27 @@
 package com.njustz.utils;
 
 public class TransactionManager {
+
+    //开启事务
+    public void beginTransaction(){
+
+    }
+
+    //提交事务
+    public void commit(){
+
+    }
+
+    //回滚事务
+    public void rollback(){
+
+    }
+
+    //释放连接
+    public void release(){
+
+    }
+
+
+
 }
